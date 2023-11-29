986d6c5a691e3a66ceea2e41d4677cbd98a5e914
victorjuwa <victor.juwa@kuda.com>
2023-11-29 02:57:20 +0100

    add axios configuration

--- src/api/apiCalls.ts
+++ src/api/apiCalls.ts
@@ -1,1 +1,52 @@
-export default {}
+import axiosInstance from "./axiosConfig";
+
+export const postCall = async (url, data, params, headerConfig) => {
+  const config = {
+    headers: {
+      ...headerConfig,
+    },
+    ...params,
+  };
+  return await axiosInstance.post(url, data, config);
+};
+
+export const getCall = async (url, params) => {
+  const config = {
+    headers: {
+    },
+    ...params,
+  };
+  return await axiosInstance.get(url, config);
+};
+
+export const postRequest = async ({ url, data }) => {
+  const config = {
+    headers: {},
+  };
+  const response = await axiosInstance.post(url, data, config);
+  return response?.data || response;
+};
+
+export const getRequest = async ({ url }) => {
+  const config = {
+    headers: {},
+  };
+  const response = await axiosInstance.get(url, config);
+  return response?.data || response;
+};
+
+export const putRequest = async ({ url, data }) => {
+  const config = {
+    headers: {},
+  };
+  const response = await axiosInstance.put(url, data, config);
+  return response?.data || response;
+};
+
+export const deleteRequest = async ({ url, data }) => {
+  const config = {
+    headers: {},
+  };
+  const response = await axiosInstance.delete(url, config);
+  return response?.data || response;
+};
--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,1 +1,47 @@
-export default {}
+import axios from "axios";
+const deleteToken = () =>{
+	localStorage.clear();
+	sessionStorage.clear()
+}
+
+const axiosInstance = axios.create({
+  baseURL: process.env.REACT_APP_NERVE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    clientId: process.env.REACT_APP_CLIENT_ID,
+  },
+});
+
+
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+   
+
+    return config;
+  },
+  (error) => {
+    return error;
+  }
+);
+
+// DELETE EXPIRED TOKEN
+axiosInstance.interceptors.response.use(
+  function (response) {
+    if (response.status === 401) {
+      deleteToken();
+    }
+
+    return response;
+  },
+  function (error) {
+    if (error?.response?.status === 401) {
+      deleteToken();
+      window.location.href = "/login";
+    } else {
+      return Promise.reject(error);
+    }
+  }
+);
+
+export default axiosInstance;
